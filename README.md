661e2b5c843b2506f56a0ae4c4a4fecd363bf6a1
GuillaumeNury <gnury@luccasoftware.com>
2023-10-04 01:02:24 +0200

    Initial commit

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+## RFC
+
+https://github.com/ngrx/platform/discussions/3796
+
+CSS : https://www.getpapercss.com/
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { catchError, map, of } from 'rxjs';
+import { PokeService } from './poke.service';
+import { PokemonType } from './poke.models';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'atelier-signal-state';
+
+  #pokeService = inject(PokeService);
+
+  pokemonTypes$ = this.#pokeService.getTypes();
+  pokemons$ = this.#pokeService.getPokemons();
+  languages$ = this.#pokeService.getLanguages();
+
+  selectedLang: string = 'en';
+  selectedType: PokemonType | null = null;
 }
